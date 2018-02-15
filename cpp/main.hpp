983cd7bbec76dbb15693fc3c953476d234c76f15
Nick Tyler <tylern@canisius.edu>
2018-02-15 12:01:23 -0500

    Fix graphs again

--- cpp/main.hpp
+++ cpp/main.hpp
@@ -67,12 +67,12 @@ TH2D *mom_vs_beta_electron = new TH2D(
     "mom_vs_beta_electron", "mom_vs_beta_electron", 500, 0, 5, 500, 0.0, 1.2);
 
 TH2D *deltat_proton =
-    new TH2D("deltat_proton", "#Deltat assuming mass of proton", 500, -1.0,
-             10.0, 500, -10.0, 10.0);
+    new TH2D("deltat_proton", "#Deltat assuming mass of proton", 500, 0.0, 10.0,
+             500, -10.0, 10.0);
 TH2D *deltat_pion = new TH2D("deltat_pion", "#Deltat assuming mass of pion",
                              500, 0.0, 10.0, 500, -10.0, 10.0);
 TH2D *deltat_electron =
-    new TH2D("deltat_electron", "#Deltat assuming mass of electron", 500, -1.0,
+    new TH2D("deltat_electron", "#Deltat assuming mass of electron", 500, 0.0,
              10.0, 500, -10.0, 10.0);
 TH2D *deltat_proton_withID =
     new TH2D("deltat_proton_withID", "#Deltat assuming mass of proton with ID",
@@ -90,8 +90,8 @@ TH2D *deltat_electron_0th =
 
 TH2D *deltat_electron_0th_ID =
     new TH2D("deltat_electron_0th_ID",
-             "#Deltat assuming mass of electron with ID at 0th", 500, -1.0,
-             10.0, 500, -10.0, 10.0);
+             "#Deltat assuming mass of electron with ID at 0th", 500, 0.0, 10.0,
+             500, -10.0, 10.0);
 
 // Calcuating Q^2
 // q^mu^2 = (e^mu - e^mu')^2 = -Q^2
