8f911cd442c906d90cc4cc51890983d6350a4e66
Nick Tyler <tylern@canisius.edu>
2018-03-07 13:35:37 -0500

    Remove unneeded references to save memory

--- cpp/deltat.cpp
+++ cpp/deltat.cpp
@@ -6,9 +6,7 @@
 #include "deltat.hpp"
 #include "constants.hpp"
 
-Delta_T::Delta_T(double sc_time, double sc_pathlength) {
-  this->vertex = vertex_time(sc_time, sc_pathlength, 1.0);
-}
+Delta_T::Delta_T(double sc_time, double sc_pathlength) { vertex = vertex_time(sc_time, sc_pathlength, 1.0); }
 
 Delta_T::~Delta_T() {}
 
@@ -18,20 +16,20 @@ double Delta_T::vertex_time(double sc_time, double sc_pathlength, double relatav
 
 void Delta_T::deltat(double momentum, double sc_t, double sc_r) {
   double beta = 0.0;
-  beta = 1.0 / sqrt(1.0 + (this->masses.at(0) / momentum) * (this->masses.at(0) / momentum));
-  this->dt_E = this->vertex - vertex_time(sc_t, sc_r, beta);
+  beta = 1.0 / sqrt(1.0 + (masses.at(0) / momentum) * (masses.at(0) / momentum));
+  dt_E = vertex - vertex_time(sc_t, sc_r, beta);
 
-  beta = 1.0 / sqrt(1.0 + (this->masses.at(1) / momentum) * (this->masses.at(1) / momentum));
-  this->dt_P = this->vertex - vertex_time(sc_t, sc_r, beta);
+  beta = 1.0 / sqrt(1.0 + (masses.at(1) / momentum) * (masses.at(1) / momentum));
+  dt_P = vertex - vertex_time(sc_t, sc_r, beta);
 
-  beta = 1.0 / sqrt(1.0 + (this->masses.at(2) / momentum) * (this->masses.at(2) / momentum));
-  this->dt_Pi = this->vertex - vertex_time(sc_t, sc_r, beta);
+  beta = 1.0 / sqrt(1.0 + (masses.at(2) / momentum) * (masses.at(2) / momentum));
+  dt_Pi = vertex - vertex_time(sc_t, sc_r, beta);
 
-  beta = 1.0 / sqrt(1.0 + (this->masses.at(3) / momentum) * (this->masses.at(3) / momentum));
-  this->dt_K = this->vertex - vertex_time(sc_t, sc_r, beta);
+  beta = 1.0 / sqrt(1.0 + (masses.at(3) / momentum) * (masses.at(3) / momentum));
+  dt_K = vertex - vertex_time(sc_t, sc_r, beta);
 }
 
-double Delta_T::Get_dt_E() { return this->dt_E; }
-double Delta_T::Get_dt_P() { return this->dt_P; }
-double Delta_T::Get_dt_Pi() { return this->dt_Pi; }
-double Delta_T::Get_dt_K() { return this->dt_K; }
+double Delta_T::Get_dt_E() { return dt_E; }
+double Delta_T::Get_dt_P() { return dt_P; }
+double Delta_T::Get_dt_Pi() { return dt_Pi; }
+double Delta_T::Get_dt_K() { return dt_K; }
