3e7a7f7082e916d869ed186b9c8561b3b654058b
Nicholas Tyler <tylern@canisius.edu>
2018-05-19 13:18:42 -0400

    Use iterator for omp

--- cpp_hipo/hipo/datahandeler.cpp
+++ cpp_hipo/hipo/datahandeler.cpp
@@ -33,9 +33,10 @@ DataHandeler::~DataHandeler() {
 
 void DataHandeler::run() {
 #pragma omp parallel for
-  for (auto file_name : input_files) {
-    std::cout << "Processing: " << file_name << std::endl;
-    file_handeler(file_name);
+  for (std::vector<std::string>::const_iterator file_name = input_files.begin(); file_name != input_files.end();
+       ++file_name) {
+    std::cout << "Processing: " << *file_name << std::endl;
+    file_handeler(*file_name);
   }
 }
 
