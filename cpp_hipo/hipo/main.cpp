01b6bdd057aa830d977e34b34e3df7c7018fb8e4
Nicholas Tyler <tylern@canisius.edu>
2018-05-19 12:10:00 -0400

    Add glob and datahandeler to hipo_cpp

--- cpp_hipo/hipo/main.cpp
+++ cpp_hipo/hipo/main.cpp
@@ -4,26 +4,32 @@
 /************************************************************************/
 
 // Only My Includes. All others in main.h
+#include "glob_files.hpp"
 #include "main.hpp"
 
-using namespace std;
-
 int main(int argc, char **argv) {
-  if (argc == 2) {
-    char *infilename = argv[1];
-    char out[] = "out.root";
-    datahandeler(infilename, out);
-  } else if (argc == 3) {
-    char *infilename = argv[1];
-    char *outfilename = argv[2];
-    datahandeler(infilename, outfilename);
-  } else {
+  std::vector<std::string> files;
+  if (argc >= 2) {
+    files = glob(argv[1]);
+    // for (auto f : files) std::cout << f << std::endl;
+  } else if (argc < 2) {
     std::cerr << RED << "Error: \n";
     std::cerr << BOLDRED << "\tNeed input file and output file\n";
     std::cerr << RESET << "Usage:\n\t";
     std::cerr << BOLDWHITE << argv[0] << " infile.root outfile.root\n\n";
     std::cerr << RESET << std::endl;
+    return 1;
   }
 
+  std::string outfilename;
+  if (argc == 2) {
+    outfilename = "out.root";
+  } else if (argc == 3) {
+    outfilename = argv[2];
+  }
+
+  DataHandeler *dh = new DataHandeler(files, outfilename);
+  dh->run();
+  delete dh;
   return 0;
 }
